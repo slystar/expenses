ef89db60f7d49bc4fdb28279f66edbea5afc993f
Sly <sylvain.lafleur@gmail.com>
2012-08-02 13:36:06 -0400

    Added some tests for backup model

--- app/models/backup.rb
+++ app/models/backup.rb
@@ -1,2 +1,8 @@
 class Backup < ActiveRecord::Base
+    # Accessible attributes
+    attr_accessible :backup_date, :backup_dir_size_KB
+
+    # Validations
+    validates :backup_date, :presence => true
+    validates :backup_dir_size_KB, :presence => true, :numericality => true
 end
--- spec/models/backup_spec.rb
+++ spec/models/backup_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Backup do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "Please review and verify if more tests are needed: #{__FILE__}"
+
+  before(:each) do
+      @attr={:backup_date => Date.today, :backup_dir_size_KB => 100}
+  end
+
+  it "should create a new instance given valid attributes" do
+      Backup.create!(@attr)
+  end
+
+  it "should require a date" do
+      missing=Backup.new(@attr.merge(:backup_date => ""))
+      missing.should_not be_valid
+  end
+
+  it "should have backup_date as a Date" do
+      missing=Backup.new(@attr.merge(:backup_date => "abc"))
+      missing.should_not be_valid
+  end
+
+  it "should require a size" do
+      missing=Backup.new(@attr.merge(:backup_dir_size_KB => ""))
+      missing.should_not be_valid
+  end
+
+  it "should have size as a number" do
+      missing=Backup.new(@attr.merge(:backup_dir_size_KB => "abc"))
+      missing.should_not be_valid
+  end
 end
