33bd65e165184a794fef920f0ec4e0c41477a3c1
Sly <sylvain.lafleur@gmail.com>
2012-08-10 13:04:22 -0400

    Added tests for Users
    Permissions will be in it's own table.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,27 @@ require 'spec_helper'
 
 describe User do
 
-    pending "add some examples to (or delete) #{__FILE__}"
-
     before(:each) do
 	@attr={:user_name => 'test', :password => 'test'}
     end
 
-    it "should be unique" do
+     it "should create a new instance given valid attributes" do
+	 User.create!(@attr)
+     end
+
+    it "should have a unique user_name" do
 	User.create!(@attr)
 	user=User.new(@attr)
 	user.should_not be_valid
     end
 
+    it "should have a user_name" do
+	user=User.new(@attr.merge(:user_name => ""))
+	user.should_not be_valid
+    end
+
+    it "should have a password_digest" do
+	user=User.new(@attr.merge(:password => ""))
+	user.should_not be_valid
+    end
 end
