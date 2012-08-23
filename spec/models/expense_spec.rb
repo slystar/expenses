50623ecd18278bee4029956ea7c882e780339da0
Sly <sylvain.lafleur@gmail.com>
2012-08-23 13:54:05 -0400

    Added pending tests to expenses

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,6 +1,25 @@
 require 'spec_helper'
 
 describe Expense do
-    pending "add some examples to (or delete) #{__FILE__}"
-    pending "Test associations"
+    pending "Review this test"
+
+    it "should have a date_purchased"
+
+    it "should have a store"
+
+    it "should have a pay_method"
+
+    it "should have a reason"
+
+    it "should have a user_id"
+
+    it "should have a group_id"
+
+    it "should only allow numbers in amount"
+
+    it "should not be created with a process date"
+
+    it "should not be created with process flag set to true"
+
+    it "should not be destroyable"
 end
