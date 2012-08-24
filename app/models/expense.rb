c2a91993a4dfd670aedfd4ecb98c1f6f0c266a74
Sly <sylvain.lafleur@gmail.com>
2012-08-24 14:14:27 -0400

    Added more tests for expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,20 @@
 class Expense < ActiveRecord::Base
+    # Accessible attributes
+    attr_accessible :date_purchased, :store_id, :pay_method_id, :reason_id, :user_id, :group_id
+
     # Relationshipts
     belongs_to :store
     belongs_to :pay_method
     belongs_to :reason
+
+    # Validations
+    validates :date_purchased, :presence => true, :custom_valid_datetime => true
+    validates :store_id, :presence => true
+    validates :pay_method_id, :presence => true
+    validates :reason_id, :presence => true
+    validates :user_id, :presence => true
+    validates :group_id, :presence => true
+    validates_presence_of :store
+    validates_presence_of :pay_method
+    validates_presence_of :reason
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,19 +1,87 @@
 require 'spec_helper'
 
 describe Expense do
-    pending "Review this test"
 
-    it "should have a date_purchased"
+    before(:each) do
+	@store=Store.create(:name => 'Future shop')
+	@reason=Reason.create(:name => 'TV')
+	@pay_method=PayMethod.create(:name => 'cash')
+	@attr={:date_purchased => Time.now, :store_id => @store.id, :pay_method_id => @pay_method.id, :reason_id => @reason.id, :user_id => 1, :group_id => 1}
+    end
 
-    it "should have a store"
+    it "should create a record with valid attributes" do
+	Expense.create!(@attr)
+    end
 
-    it "should have a pay_method"
+    it "should have a date_purchased" do
+	expense=Expense.new(@attr.merge(:date_purchased => nil))
+	expense.should_not be_valid
+    end
 
-    it "should have a reason"
+    it "should accept a date for date_purchased" do
+	expense=Expense.new(@attr.merge(:date_purchased => '2011-10-14'))
+	expense.should be_valid
+    end
 
-    it "should have a user_id"
+    it "should refuse invalid dates for date_purchased" do
+	expense=Expense.new(@attr.merge(:date_purchased => '2011 10 14 aaa'))
+	expense.should_not be_valid
+    end
 
-    it "should have a group_id"
+    it "should have a store" do
+	expense=Expense.new(@attr.merge(:store_id => nil))
+	expense.should_not be_valid
+    end
+
+    it "should have an existing store" do
+	expense=Expense.new(@attr)
+	expense.store_id=9990000000
+	expense.should_not be_valid
+    end
+
+    it "should have a pay_method" do
+	expense=Expense.new(@attr.merge(:pay_method_id => nil))
+	expense.should_not be_valid
+    end
+
+    it "should have an existing pay_method" do
+	expense=Expense.new(@attr)
+	expense.pay_method_id=9990000000
+	expense.should_not be_valid
+    end
+
+    it "should have a reason" do
+	expense=Expense.new(@attr.merge(:reason_id => nil))
+	expense.should_not be_valid
+    end
+
+    it "should have an existing reason" do
+	expense=Expense.new(@attr)
+	expense.reason_id=9990000000
+	expense.should_not be_valid
+    end
+
+    it "should have a user_id" do
+	expense=Expense.new(@attr.merge(:user_id => nil))
+	expense.should_not be_valid
+    end
+
+    pending "should map to an existing user" do
+	expense=Expense.new(@attr)
+	expense.user_id=9990000000
+	expense.should_not be_valid
+    end
+
+    it "should have a group_id" do
+	expense=Expense.new(@attr.merge(:group_id => nil))
+	expense.should_not be_valid
+    end
+
+    pending "should map to an existing group" do
+	expense=Expense.new(@attr)
+	expense.group_id=9990000000
+	expense.should_not be_valid
+    end
 
     it "should have an amount"
 
@@ -26,4 +94,6 @@ describe Expense do
     it "should not be destroyable if it's been processed"
 
     it "should be destroyable if it hasn't been processed"
+
+    pending "Review this test"
 end
