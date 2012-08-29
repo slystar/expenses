a4c3340a4b4efda9e166df558599cea528d578b6
Sly <sylvain.lafleur@gmail.com>
2012-08-29 12:46:10 -0400

    Added tests for GroupMember model

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,2 +1,14 @@
 class GroupMember < ActiveRecord::Base
+    # Accessible attributes
+    attr_accessible :user_id, :group_id
+
+    # Relationshipts
+    belongs_to :user
+    belongs_to :group
+
+    # Validations
+    validates :user_id, :presence => true
+    validates :group_id, :presence => true, :uniqueness => {:scope => :user_id}
+    validates_presence_of :user
+    validates_presence_of :group
 end
--- spec/models/group_member_spec.rb
+++ spec/models/group_member_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 describe GroupMember do
-  pending "add some examples to (or delete) #{__FILE__}"
+    def create_group
+	group=Group.create!(:name => Faker::Company.name)
+    end
+
+    before(:each) do
+	@user=User.create!(:user_name => Faker::Name.name, :password => 'test')
+	@group=create_group
+	@attr={:user_id => @user, :group_id => @group}
+    end
+
+    it "should create a new instance given valid attributes" do
+	GroupMember.create!(@attr)
+    end
+
+    it "should require a user_id" do
+	gm=GroupMember.new(@attr.merge(:user_id => nil))
+	gm.should_not be_valid
+    end
+
+    it "should require a group_id" do
+	gm=GroupMember.new(@attr.merge(:group_id => nil))
+	gm.should_not be_valid
+    end
+
+    it "should not allow a user to be in the same group more than once" do
+	gm=GroupMember.create!(@attr)
+	gm2=GroupMember.new(@attr)
+	gm2.should_not be_valid
+    end
+
+    it "should allow a user to be in different groups" do
+	group2=create_group
+	gm=GroupMember.create!(@attr)
+	gm2=GroupMember.new(@attr.merge(:group_id => group2.id))
+	gm2.should be_valid
+    end
+
+    it "should map to a valid user" do
+	gm=GroupMember.new(@attr.merge(:user_id => 99999))
+	gm.should_not be_valid
+    end
+
+    it "should map to a vlid group" do
+	gm=GroupMember.new(@attr.merge(:group_id => 99999))
+	gm.should_not be_valid
+    end
 end
