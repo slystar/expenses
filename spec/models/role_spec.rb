6dd29840959a34b9f6d00abb07dac6c0e9385f25
Sly <sylvain.lafleur@gmail.com>
2012-10-17 13:10:19 -0400

    Added tests for "Role" and "UserRole"

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Role do
+
+    before(:each) do
+	@attr={:name => "admin_role", :description => "Test Admin role"}
+    end
+
+    pending "should create a new instance given valid attributes" do
+    end
+
+    pending "should require a name" do
+    end
+
+    pending "should require a description" do
+    end
+
+    pending "should have a unique name" do
+    end
+    
+    pending "should be destroyable if no users are assigned to it" do
+    end
+
+    pending "should not be destroyable if users are assigned to it" do
+    end
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe UserRole do
+
+    before(:each) do
+	@user=User.create!(:user_name => Faker::Name.name, :password => 'testuserrole')
+	@role=Role.create!(:name => Faker::Name.name, :description => 'Test user role')
+	@attr={:user_id => @user.id, :role_id => @role.id}
+    end
+
+    pending "should create a new instance given valid attributes" do
+    end
+    pending "should not allow users to be in the same role more than once" do
+    end
+    
+    pending "should allow users to be in multiple roles" do
+    end
+
+    pending "should map to a vlide user" do
+    end
+
+    pending "should map to a vlide role" do
+    end
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
