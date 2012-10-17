d3d1bdb89a3761eef79f35369a654231cfdb4728
Sly <sylvain.lafleur@gmail.com>
2012-10-17 12:52:42 -0400

    Adde Role table to hold roles for permissions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120828171019) do
+ActiveRecord::Schema.define(:version => 20121017165043) do
 
   create_table "backups", :force => true do |t|
     t.datetime "backup_date"
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(:version => 20120828171019) do
 
   create_table "reasons", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
