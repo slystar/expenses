074b966ed80b67d01737575533f6a401bbedcfb2
Sly <sylvain.lafleur@gmail.com>
2013-02-06 13:04:32 -0500

    Fixed some tests for ImportHistory

--- app/models/import_history.rb
+++ app/models/import_history.rb
@@ -1,2 +1,25 @@
 class ImportHistory < ActiveRecord::Base
+    # Accessible attributes
+    attr_accessible :import_config_id, :original_file_name
+
+    # Relationships
+    #has_many :expenses
+
+    # Validations
+    validates :user_id, :presence => true
+    validates :import_config_id, :presence => true
+
+    # Class method to generate new filename
+    def self.generate_new_file_name(original_file_name)
+	# Get current datetime
+	now=Time.now
+	# Data to hash
+	data="#{now}#{original_file_name}"
+	# Calculate hash of original name, can later be changed to file contents
+	hash=Digest::MD5.hexdigest(data)
+	# Prepare filename
+	new_name="#{now.year}_#{now.month}_#{now.day}_#{hash}"
+	# Return new name
+	return new_name
+    end
 end
--- spec/models/import_history_spec.rb
+++ spec/models/import_history_spec.rb
@@ -3,22 +3,38 @@ require 'spec_helper'
 describe ImportHistory do
 
     before(:each) do
-	@attr={:user_id => 1, :import_config_id => 1, :original_file_name = "uploaded_file.csv"}
+	@attr={:user_id => 1, :import_config_id => 1, :original_file_name => "uploaded_file.csv"}
+    end
+
+    def get_valid_import_history(attr=@attr)
+	ih=ImportHistory.new(attr)
+	# Add user id because it should not be mass assignable
+	ih.user_id=attr[:user_id]
+	# Add new_file_name
+	ih.new_file_name=ImportHistory.generate_new_file_name(ih.original_file_name)
+	# Return object
+	return ih
     end
 
     it "should create a new instance given valid attributes" do
-	ImportHistory.create!(@attr)
+	ih=get_valid_import_history
+	# Try to save
+	ih.save!
     end
 
     it "should require a user_id" do
-	ih=ImportHistory.new(@attr.merge(:user_id => nil ))
+	ih=get_valid_import_history(@attr.merge(:user_id => nil ))
 	ih.should_not be_valid
     end
     
-    pending "should require an import_config" do
+    it "should require an import_config_id" do
+	ih=get_valid_import_history(@attr.merge(:import_config_id => nil ))
+	ih.should_not be_valid
     end
 
-    pending "should require a valid import_config_id" do
+    it "should require a valid import_config_id" do
+	ih=get_valid_import_history(@attr.merge(:import_config_id => 99999 ))
+	ih.should_not be_valid
     end
 
     pending "should require the original file name" do
