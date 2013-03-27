e512b8bf4fd68b2c694f322328967ff9bb54acea
Sly <sylvain.lafleur@gmail.com>
2013-03-27 15:00:56 -0400

    Added ability to call binding.pry to debug easier

--- lib/legacy_migration/helper.rb
+++ lib/legacy_migration/helper.rb
@@ -1,12 +1,17 @@
 module LegacyHelper
     # Method to raise error
-    def raise_error(field,old,new)
+    def raise_error(field,old,new,bind_with_pry=false)
 	puts('-' * 10)
 	puts("Imported data does not match old data")
 	puts("Field: #{field}")
 	p old
 	p new
 	puts('-' * 10)
+	# Bind with pry to debug if required
+	if bind_with_pry
+	    binding.pry
+	end
+	# Raise error
 	raise("Exiting, import failed")
     end
 end
