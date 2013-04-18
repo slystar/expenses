48b48dcbf77db3270220b41896b735f5ad785346
Sly <sylvain.lafleur@gmail.com>
2013-04-18 12:52:50 -0400

    Clean up describes in Sessions requests

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -16,41 +16,45 @@ describe "Sessions" do
 	return User.new({:user_name => 'test_user',:password => '1234abcd', :name => 'test user'})
     end
 
-    it "should login with valid attributes" do
-	# Fill in info
-	page.fill_in "user_name", with: @user.user_name
-	page.fill_in "password", with: @user.password
-	# Click button to submit
-	page.click_button "Log in"
-	# Test: Should redirect to menu
-	current_path.should == menu_path
-	# Test: Should have message
-	page.should have_content('Logged in')
+    describe "new, create" do
+	it "should login with valid attributes" do
+	    # Fill in info
+	    page.fill_in "user_name", with: @user.user_name
+	    page.fill_in "password", with: @user.password
+	    # Click button to submit
+	    page.click_button "Log in"
+	    # Test: Should redirect to menu
+	    current_path.should == menu_path
+	    # Test: Should have message
+	    page.should have_content('Logged in')
+	end
+
+	it "should not login with bad attributes" do
+	    # Fill in info
+	    page.fill_in "user_name", with: @user.user_name
+	    page.fill_in "password", with: @user.password + '1'
+	    # Click button to submit
+	    page.click_button "Log in"
+	    # Test: Should redirect to menu
+	    current_path.should == '/sessions'
+	    # Test: Should have message
+	    page.should have_content('Invalid user/password combination')
+	end
     end
 
-    it "should not login with bad attributes" do
-	# Fill in info
-	page.fill_in "user_name", with: @user.user_name
-	page.fill_in "password", with: @user.password + '1'
-	# Click button to submit
-	page.click_button "Log in"
-	# Test: Should redirect to menu
-	current_path.should == '/sessions'
-	# Test: Should have message
-	page.should have_content('Invalid user/password combination')
-    end
-
-    it "should logout" do
-	# Fill in info
-	page.fill_in "user_name", with: @user.user_name
-	page.fill_in "password", with: @user.password
-	# Click button to submit
-	page.click_button "Log in"
-	# Test: Should redirect to menu
-	current_path.should == menu_path
-	# Click logout
-	page.click_link 'Logout'
-	# Test: Should redirect to menu
-	current_path.should == login_path
+    describe 'destroy' do
+	it "should logout" do
+	    # Fill in info
+	    page.fill_in "user_name", with: @user.user_name
+	    page.fill_in "password", with: @user.password
+	    # Click button to submit
+	    page.click_button "Log in"
+	    # Test: Should redirect to menu
+	    current_path.should == menu_path
+	    # Click logout
+	    page.click_link 'Logout'
+	    # Test: Should redirect to menu
+	    current_path.should == login_path
+	end
     end
 end
