d7fde9e1e50b7c10c68d9826e5d1e38c6de473c8
Sly <sylvain.lafleur@gmail.com>
2013-05-09 12:59:24 -0400

    Fixed failing requests specs

--- spec/requests/reasons_spec.rb
+++ spec/requests/reasons_spec.rb
@@ -1,11 +1,58 @@
 require 'spec_helper'
 
 describe "Reasons" do
-  describe "GET /reasons" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get reasons_path
-      response.status.should be(200)
+    before(:each) do
+	@add_path="#{reasons_path}/new"
     end
-  end
+    describe 'requires login for' do
+
+	it "new" do
+	    # Visit page
+	    visit @add_path
+	    # Test
+	    current_path.should == login_path
+	end
+
+    end
+
+    describe 'after login' do
+
+	before(:each) do
+	    @new_user_id=0
+	    # Get user
+	    @user=get_next_user
+	    # Create user
+	    @user.save!
+	    # Login
+	    login_user(@user)
+	end
+
+	def login_user(user)
+	    # Visit login page
+	    visit login_path
+	    # Fill in info
+	    page.fill_in "user_name", with: user.user_name
+	    page.fill_in "password", with: user.password
+	    # Click button to submit
+	    page.click_button "Log in"
+	end
+
+	describe "add" do
+	    before(:each) do
+		visit @add_path
+	    end
+
+	    it "should add a valid record" do
+		# Get expense object
+		e=get_valid_expense
+		page.fill_in "reason_name", with: 'zzz'
+		# Test:
+		lambda{
+		    page.click_button "Save"
+		}.should change(Reason,:count).by(1)
+		# Test: should redirect to add expense
+		current_path.should == "#{expenses_path}/new"
+	    end
+	end
+    end
 end
--- spec/requests/stores_spec.rb
+++ spec/requests/stores_spec.rb
@@ -1,98 +1,58 @@
 require 'spec_helper'
 
 describe "Stores:" do
-
     before(:each) do
-	@stores=[]
-	names=["Future Shop","Best Buy", "New Egg"]
-	names.each{|name| @stores.push(Store.create!(:name => name))}
+	@add_path="#{stores_path}/new"
+    end
+    describe 'requires login for' do
+
+	it "new" do
+	    # Visit page
+	    visit @add_path
+	    # Test
+	    current_path.should == login_path
+	end
+
     end
 
-    describe "Controller methods:" do
+    describe 'after login' do
 
-	it "index" do
-	    visit "#{stores_path}"
-	    @stores.each do |store|
-		page.should have_content(store.name)
+	before(:each) do
+	    @new_user_id=0
+	    # Get user
+	    @user=get_next_user
+	    # Create user
+	    @user.save!
+	    # Login
+	    login_user(@user)
+	end
+
+	def login_user(user)
+	    # Visit login page
+	    visit login_path
+	    # Fill in info
+	    page.fill_in "user_name", with: user.user_name
+	    page.fill_in "password", with: user.password
+	    # Click button to submit
+	    page.click_button "Log in"
+	end
+
+	describe "add" do
+	    before(:each) do
+		visit @add_path
 	    end
-	    page.should have_content("New Store")
-	end
 
-	it "show" do
-	    store=@stores.first
-	    visit "#{stores_path}/#{store.id}"
-	    page.should have_content(store.name)
-	    page.should have_content("Edit")
-	    page.should have_content("Back")
-	end
-
-	it "edit" do
-	    id=1
-	    name="Canadian Tire"
-	    visit "#{stores_path}/#{id}/edit"
-	    fill_in "Name", :with => name
-	    click_on "Update Store"
-	    page.should have_content(name)
-	    page.should have_selector('a', :visible => "#{stores_path}/#{id}/edit")
-	    page.should have_selector('a', :visible => "#{stores_path}")
-	    page.should have_selector("div#flash_notice")
-	    current_path.should == "#{stores_path}/#{id}"
-	end
-
-	it "create" do
-	    stores_count=Store.find(:all).count
-	    name="Test"
-	    visit "#{stores_path}/new"
-	    fill_in "Name", :with => name
-	    click_on "Create Store"
-	    page.should have_content(name)
-	    page.should have_content("Edit")
-	    page.should have_content("Back")
-	    page.should have_selector("div#flash_notice")
-	    current_path.should == "#{stores_path}/#{stores_count + 1}"
-	    #page.driver.browser.switch_to.alert.accept
-	end
-
-	it "destroy" do
-	    id=2
-	    visit stores_path
-	    start_count=all('a').select { |a| a[:href] =~ /#{stores_path}\/[0-9]\/edit/ }.count
-	    rack_test_session_wrapper = Capybara.current_session.driver
-	    rack_test_session_wrapper.delete "#{stores_path}/#{id}"
-	    visit stores_path
-	    end_count=all('a').select { |a| a[:href] =~ /#{stores_path}\/[0-9]\/edit/ }.count
-	    end_count.should == (start_count - 1)
-	    link=all('a').select{|a| a[:href] == "#{stores_path}/#{id}/edit"}.first
-	    link.should be_nil
-	end
-    end
-
-    describe "custom tests:" do
-	it "gets the page successfully" do
-	    get stores_path
-	    response.status.should be(200)
-	end
-
-	it "should show an error when create a duplicate store" do
-	    name=@stores.first.name
-	    visit "#{stores_path}/new"
-	    fill_in "Name", :with => name
-	    click_on "Create Store"
-	    page.should have_selector("div#error_explanation")
-	    current_path.should == "#{stores_path}"
-	end
-
-	it "should show an error when renaming to a duplicate store" do
-	    id=1
-	    existing_name=Store.find(id).name
-	    name=@stores[1].name
-	    name.should_not == existing_name
-	    visit "#{stores_path}/#{id}/edit"
-	    fill_in "Name", :with => name
-	    click_on "Update Store"
-	    page.should have_selector('a', :visible => "#{stores_path}")
-	    page.should have_selector("div#error_explanation")
-	    current_path.should == "#{stores_path}/#{id}"
+	    it "should add a valid record" do
+		# Get expense object
+		e=get_valid_expense
+		page.fill_in "store_name", with: 'zzz'
+		# Test:
+		lambda{
+		    page.click_button "Save"
+		}.should change(Store,:count).by(1)
+		# Test: should redirect to add expense
+		current_path.should == "#{expenses_path}/new"
+	    end
 	end
     end
 end
