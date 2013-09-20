05b2f88cf8f24d060eff902fb9aea9240cd1bf93
Sly <sylvain.lafleur@gmail.com>
2013-09-20 12:37:25 -0400

    Now able to add user payment

--- app/controllers/user_payments_controller.rb
+++ app/controllers/user_payments_controller.rb
@@ -24,9 +24,9 @@ class UserPaymentsController < ApplicationController
   # GET /user_payments/new
   # GET /user_payments/new.json
   def new
+      # Get objects
     @user_payment = UserPayment.new
-    @users=User.where("id <> ?",current_user.id)
-    @payment_note=PaymentNote.new
+    @users=User.where("id <> ?",current_user.id).all
 
     respond_to do |format|
       format.html # new.html.erb
@@ -43,10 +43,25 @@ class UserPaymentsController < ApplicationController
   # POST /user_payments.json
   def create
     @user_payment = UserPayment.new(params[:user_payment])
+    @payment_note = PaymentNote.new(params[:payment_note])
+    @users=User.where("id <> ?",current_user.id).all
+
+    # Set defaults
+    @user_payment.from_user_id=current_user.id
 
     respond_to do |format|
       if @user_payment.save
-        format.html { redirect_to @user_payment, notice: 'User payment was successfully created.' }
+	  # Check if there is a note to save
+	  if @payment_note.note.size > 0
+	      # Reload
+	      @user_payment.reload
+	      # Set PaymentNote options
+	      @payment_note.user_payment_id=@user_payment.id
+	      @payment_note.user_id=current_user.id
+	      # Save note
+	      @payment_note.save!
+	  end
+        format.html { redirect_to menu_path, notice: 'User payment was successfully created.' }
         format.json { render json: @user_payment, status: :created, location: @user_payment }
       else
         format.html { render action: "new" }
