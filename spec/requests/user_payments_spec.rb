40cc717a5bd8d717e34a7e37281b9620627d77cd
Sly <sylvain.lafleur@gmail.com>
2013-10-10 14:30:37 -0400

    Add stubed current_user method for Request spec.

--- spec/requests/user_payments_spec.rb
+++ spec/requests/user_payments_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe "UserPayments" do
+    before(:each) do
+	# Login a user
+	@new_user_id=0
+	@user=get_next_user
+	ApplicationController.any_instance.stub(:current_user).and_return(@user)
+    end
   describe "GET /user_payments" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
