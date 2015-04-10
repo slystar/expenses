ad10ca3c3d348b21a81974cd8dc8a7313f205b24
root@antman <sylvain.lafleur@gmail.com>
2015-04-10 13:14:15 -0400

    Make Return controller spec pass tests
    Return is clearly not a good name for a model, as it clashes with the
    'return' statement in ruby. Too late now, will work around this issue as
    ruby is smart enough when it sees 'Return' or '@return' instead of just
    'return'

--- spec/controllers/returns_controller_spec.rb
+++ spec/controllers/returns_controller_spec.rb
@@ -20,11 +20,20 @@ require 'spec_helper'
 
 describe ReturnsController do
 
+    before(:each) do
+	# Login a user
+	@new_user_id=0
+	user=get_next_user
+	request.session[:user_id] = user.id
+	@exp=get_valid_expense
+    end
+
+
   # This should return the minimal set of attributes required to create a valid
   # Return. As you add validations to Return, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {  }
+      {:description => Faker::Company.name, :expense_id => @exp.id, :transaction_date => Date.today, :user_id => @exp.user_id, :amount => @exp.amount - 1}
   end
 
   # This should return the minimal set of values that should be in the session
@@ -36,32 +45,32 @@ describe ReturnsController do
 
   describe "GET index" do
     it "assigns all returns as @returns" do
-      return = Return.create! valid_attributes
-      get :index, {}, valid_session
-      assigns(:returns).should eq([return])
+      return_obj = Return.create! valid_attributes
+      get :index
+      assigns(:returns).should eq([return_obj])
     end
   end
 
   describe "GET show" do
     it "assigns the requested return as @return" do
-      return = Return.create! valid_attributes
-      get :show, {:id => return.to_param}, valid_session
-      assigns(:return).should eq(return)
+      return_obj = Return.create! valid_attributes
+      get :show, {:id => return_obj.to_param}
+      assigns(:return).should eq(return_obj)
     end
   end
 
   describe "GET new" do
     it "assigns a new return as @return" do
-      get :new, {}, valid_session
+      get :new
       assigns(:return).should be_a_new(Return)
     end
   end
 
   describe "GET edit" do
     it "assigns the requested return as @return" do
-      return = Return.create! valid_attributes
-      get :edit, {:id => return.to_param}, valid_session
-      assigns(:return).should eq(return)
+      return_obj = Return.create! valid_attributes
+      get :edit, {:id => return_obj.to_param}
+      assigns(:return).should eq(return_obj)
     end
   end
 
@@ -69,18 +78,18 @@ describe ReturnsController do
     describe "with valid params" do
       it "creates a new Return" do
         expect {
-          post :create, {:return => valid_attributes}, valid_session
+          post :create, {:return => valid_attributes}
         }.to change(Return, :count).by(1)
       end
 
       it "assigns a newly created return as @return" do
-        post :create, {:return => valid_attributes}, valid_session
+        post :create, {:return => valid_attributes}
         assigns(:return).should be_a(Return)
         assigns(:return).should be_persisted
       end
 
       it "redirects to the created return" do
-        post :create, {:return => valid_attributes}, valid_session
+        post :create, {:return => valid_attributes}
         response.should redirect_to(Return.last)
       end
     end
@@ -89,14 +98,14 @@ describe ReturnsController do
       it "assigns a newly created but unsaved return as @return" do
         # Trigger the behavior that occurs when invalid params are submitted
         Return.any_instance.stub(:save).and_return(false)
-        post :create, {:return => {  }}, valid_session
+        post :create, {:return => {  }}
         assigns(:return).should be_a_new(Return)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Return.any_instance.stub(:save).and_return(false)
-        post :create, {:return => {  }}, valid_session
+        post :create, {:return => {  }}
         response.should render_template("new")
       end
     end
@@ -105,42 +114,42 @@ describe ReturnsController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested return" do
-        return = Return.create! valid_attributes
+        return_obj = Return.create! valid_attributes
         # Assuming there are no other returns in the database, this
         # specifies that the Return created on the previous line
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
         Return.any_instance.should_receive(:update_attributes).with({ "these" => "params" })
-        put :update, {:id => return.to_param, :return => { "these" => "params" }}, valid_session
+        put :update, {:id => return_obj.to_param, :return => { "these" => "params" }}
       end
 
       it "assigns the requested return as @return" do
-        return = Return.create! valid_attributes
-        put :update, {:id => return.to_param, :return => valid_attributes}, valid_session
-        assigns(:return).should eq(return)
+        return_obj = Return.create! valid_attributes
+        put :update, {:id => return_obj.to_param, :return => valid_attributes}
+        assigns(:return).should eq(return_obj)
       end
 
       it "redirects to the return" do
-        return = Return.create! valid_attributes
-        put :update, {:id => return.to_param, :return => valid_attributes}, valid_session
-        response.should redirect_to(return)
+        return_obj = Return.create! valid_attributes
+        put :update, {:id => return_obj.to_param, :return => valid_attributes}
+        response.should redirect_to(return_obj)
       end
     end
 
     describe "with invalid params" do
       it "assigns the return as @return" do
-        return = Return.create! valid_attributes
+        return_obj = Return.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Return.any_instance.stub(:save).and_return(false)
-        put :update, {:id => return.to_param, :return => {  }}, valid_session
-        assigns(:return).should eq(return)
+        put :update, {:id => return_obj.to_param, :return => {  }}
+        assigns(:return).should eq(return_obj)
       end
 
       it "re-renders the 'edit' template" do
-        return = Return.create! valid_attributes
+        return_obj = Return.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Return.any_instance.stub(:save).and_return(false)
-        put :update, {:id => return.to_param, :return => {  }}, valid_session
+        put :update, {:id => return_obj.to_param, :return => {  }}
         response.should render_template("edit")
       end
     end
@@ -148,15 +157,15 @@ describe ReturnsController do
 
   describe "DELETE destroy" do
     it "destroys the requested return" do
-      return = Return.create! valid_attributes
+      return_obj = Return.create! valid_attributes
       expect {
-        delete :destroy, {:id => return.to_param}, valid_session
+        delete :destroy, {:id => return_obj.to_param}
       }.to change(Return, :count).by(-1)
     end
 
     it "redirects to the returns list" do
-      return = Return.create! valid_attributes
-      delete :destroy, {:id => return.to_param}, valid_session
+      return_obj = Return.create! valid_attributes
+      delete :destroy, {:id => return_obj.to_param}
       response.should redirect_to(returns_url)
     end
   end
